8429a54f23cc2e89cccef102a7978fbe811c7280
Suvadip <suvadip.gosh@mheducation.com>
2017-11-15 23:27:33 +0530

    some beautification

--- js/filter-list.js
+++ js/filter-list.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".filter-list .filter-item").on('click',function(e) {
     var filterType = $(this).attr("value");
+    $(".filter-list .filter-item").removeClass('active-filter');
+    $(this).addClass('active-filter');
     $('.filter-section').removeClass('is-active');
     $('#'+filterType+'.filter-section').addClass('is-active');
   });
